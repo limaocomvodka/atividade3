390d5f9c3a24f022ca6290c66ff6dd62b1734617
limaocomvodka <104787948+limaocomvodka@users.noreply.github.com>
2022-07-08 10:07:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//for
+for (let x=1; x<=10; x++){
+console.log (x)
+}
+
+//while
+let y=10
+while (y>=1){
+console.log (y)
+y--
+}
+
+//impares
+for(let impares=1; impares<=100; impares +=2){
+    console.log(impares)
+}
+
+//pares
+for(let p=0, p<=100; p+=2){
+    console.log(p)
+}
